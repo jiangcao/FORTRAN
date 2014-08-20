a1baca7c2dda21d7993489c5f421b8e4e8779e8b
Jiang Cao <jiang.cao89@gmail.com>
2014-08-20 09:53:17 +0200

    add sparse_real for real sparse matrix

--- testsparse.f90
+++ testsparse.f90
@@ -1,6 +1,5 @@
 program sparse_test 
-use list 
-use sparse 
+use sparse_real 
 implicit none 
 integer :: i,j
 real(8) :: b
@@ -8,70 +7,70 @@ real(8),allocatable :: x(:),a(:)
 integer,allocatable :: ja(:),ia(:)
 logical :: find
 
-type(sparse_lil_t),pointer :: sA
+type(sparse_t),pointer :: sA
 
 
 write(*,*) "Test Sparse module"
 
-call sparse_lil(sA,5,5,axe='r')
-write(*,*) 'nnz',sparse_lil_nnz(sA)
+sA => sparse_create(5,5,axe='r')
+write(*,*) 'nnz',sparse_nnz(sA)
 
 
 
 
 
 write(*,*) 'put'
-call rsparse_lil_put(sA,1,1,num=1.0d0)
-call rsparse_lil_put(sA,1,2,num=-1.0d0)
-call rsparse_lil_put(sA,1,4,num=-3.0d0)
-call rsparse_lil_put(sA,2,1,num=-2.0d0)
-call rsparse_lil_put(sA,2,2,num=5.0d0)
-call rsparse_lil_put(sA,3,3,num=4.0d0)
-call rsparse_lil_put(sA,3,4,num=6.0d0)
-call rsparse_lil_put(sA,3,5,num=4.0d0)
-call rsparse_lil_put(sA,4,1,num=-4.0d0)
-call rsparse_lil_put(sA,4,3,num=2.0d0)
-call rsparse_lil_put(sA,4,4,num=7.0d0)
-call rsparse_lil_put(sA,5,2,num=8.0d0)
-call rsparse_lil_put(sA,5,5,num=-5.0d0)
+call sparse_put(sA,1,1,1.0d0)
+call sparse_put(sA,1,2,-1.0d0)
+call sparse_put(sA,1,4,-3.0d0)
+call sparse_put(sA,2,1,-2.0d0)
+call sparse_put(sA,2,2,5.0d0)
+call sparse_put(sA,3,3,4.0d0)
+call sparse_put(sA,3,4,6.0d0)
+call sparse_put(sA,3,5,4.0d0)
+call sparse_put(sA,4,1,-4.0d0)
+call sparse_put(sA,4,3,2.0d0)
+call sparse_put(sA,4,4,7.0d0)
+call sparse_put(sA,5,2,8.0d0)
+call sparse_put(sA,5,5,-5.0d0)
 
 write(*,*)'get'
 do i=1,5
    do j=1,5
-      b = rsparse_lil_get(sA,i,j,find=find)
+      b = sparse_get(sA,i,j,find=find)
       write(*,*) i,j,b, find
    end do
 End do
 
 write(*,*)'get rows'
-allocate(x(sparse_lil_size(sA,2)))
+allocate(x(sparse_size(sA,2)))
 do i=1,5
-   call rsparse_lil_getRow(sA,i,x)
+   call sparse_Row(sA,i,x)
    write(*,*) x
    write(*,*)
 end do
 deallocate(x)
 
 write(*,*)'get cols'
-allocate(x(sparse_lil_size(sA,1)))
+allocate(x(sparse_size(sA,1)))
 do i=1,5
-   call rsparse_lil_getCol(sA,i,x)
+   call sparse_Col(sA,i,x)
    write(*,*) x
    write(*,*)
 end do
 deallocate(x)
 
 write(*,*)'To CSR format'
-allocate(a(sparse_lil_nnz(sA)))
-allocate(ia(sparse_lil_nnz(sA)))
-allocate(ja(sparse_lil_size(sA,1)+1))
-call rsparse_lil_to_csr(sA,a,ia,ja)
+allocate(a(sparse_nnz(sA)))
+allocate(ia(sparse_nnz(sA)))
+allocate(ja(sparse_size(sA,1)+1))
+call sparse_to_csr(sA,a,ia,ja)
 write(*,*) 'values = ',a
 write(*,*) 'columns = ',ia
 write(*,*) 'rowIndex = ',ja
 deallocate(a,ia,ja)
 
 write(*,*) "Free"
-call sparse_lil_free(sA)
+call sparse_free(sA)
 write(*,*) "End Test"
 end program sparse_test
